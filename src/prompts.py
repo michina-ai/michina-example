2f076137c1329adebee79348d49da9123fa66dda
bkitano <briankitano@gmail.com>
2023-07-24 11:59:45 -0700

    adding toneCheck example

--- src/prompts.py
+++ src/prompts.py
@@ -7,7 +7,7 @@ import os
 dotenv.load_dotenv()
 
 
-def check_for_political_content(product: str) -> str:
+def check_for_political_content(message: str) -> str:
     llm = ChatOpenAI(
         temperature=0,
         model="gpt-3.5-turbo",
@@ -20,4 +20,19 @@ You are deciding whether a message contains political content. The message is: "
     prompt = PromptTemplate.from_template(template)
     chain = LLMChain(llm=llm, prompt=prompt)
 
-    return chain.run(product)
+    return chain.run(message)
+
+def respond_to_customer(message: str) -> str:
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-3.5-turbo",
+        openai_api_key=os.environ["OPENAI_API_KEY"],
+    )
+
+    template = """\
+You are responding to a customer message. The message is: "{message}". Write a response.
+"""
+    prompt = PromptTemplate.from_template(template)
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    return chain.run(message)
--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -1,12 +1,32 @@
 from unittest.mock import patch
-from src.prompts import check_for_political_content
+from src.prompts import check_for_political_content, respond_to_customer
 
-from michina import isConsistent
+from michina import isConsistent, toneCheck
 
-
-def test_check_for_political_content():
-    message = "I want to buy socks."
+"""
+This is a consistency check. It tests whether your prompt's output 
+is consistent with the goal of the test itself.
+"""
+def test_check_for_political_content_consistent():
+    message = "I want to buy a campaign poster for Obama."
     statement = check_for_political_content(message)
-    # we want the response to be topical.
     response = isConsistent(message, statement)
     assert response.judgment > 0.5
+
+def test_respond_to_customer():
+    customer_message = "I want to buy a campaign poster for Obama."
+    response_message = respond_to_customer(customer_message)
+
+    tone_check = toneCheck(response_message, "polite")
+    assert tone_check.judgment > 0.5
+
+def test_respond_to_customer_fail():
+    """
+        this one will fail because the tone doesn't match.
+    """
+    customer_message = "I want to buy a campaign poster for Obama."
+    response_message = respond_to_customer(customer_message)
+
+    tone_check = toneCheck(response_message, "angry")
+    assert tone_check.judgment > 0.5
+    
