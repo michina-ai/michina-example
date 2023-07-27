1abcc2ca4f211e34468a9b5c16fed6aa8fb3578c
bkitano <briankitano@gmail.com>
2023-07-26 17:09:43 -0700

    updating michina version

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -1,5 +1,16 @@
 from src.prompts import check_for_political_content, respond_to_customer
 from michina.checks import ToneCheck, ConsistencyCheck
+from os import environ as env
+# import michina
+
+michina_config = {
+    'model': 'gpt-3.5-turbo-16k-0613',
+    'temperature': 0,
+    'openai_api_key': env["OPENAI_API_KEY"],
+}
+
+tone = ToneCheck(**michina_config)
+consistency = ConsistencyCheck(**michina_config)
 
 """
 This is a consistency check. It tests whether your prompt's output 
@@ -8,23 +19,20 @@ is consistent with the goal of the test itself.
 def test_check_for_political_content_consistent():
     message = "I want to buy a campaign poster for Obama."
     statement = check_for_political_content(message)
-    response = ConsistencyCheck.check(message, statement)
+    response = consistency.check(message, statement)
     assert response.judgment > 0.5
 
 def test_respond_to_customer():
     customer_message = "I want to buy a campaign poster for Obama."
     response_message = respond_to_customer(customer_message)
 
-    tone_check = ToneCheck.check(response_message, "polite")
+    tone_check = tone.check(response_message, "polite")
     assert tone_check.judgment > 0.5
 
 def test_respond_to_customer_fail():
-    """
-        this one will fail because the tone doesn't match.
-    """
     customer_message = "I want to buy a campaign poster for Obama."
     response_message = respond_to_customer(customer_message)
 
-    tone_check = ToneCheck.check(response_message, "angry")
+    tone_check = tone.check(response_message, "angry")
     assert tone_check.judgment < 0.5
     
